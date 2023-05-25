1997cd9e1416c268a4f4e5efb3fc90274ae3b571
diya-mirji <dmirji@uci.edu>
2023-05-25 13:51:21 -0700

    Initial Commit

---
+++ CampusCoachUI/CampusCoachUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusCoachUIApp.swift
+//  CampusCoachUI
+//
+//  Created by Diya V Mirji on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CampusCoachUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
